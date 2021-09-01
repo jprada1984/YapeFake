85f30f629e2fcb20c0bde23d5e79ec7318f49eb8
acidcoolffc <acidcool_ffc@hotmail.com>
2021-08-31 23:33:53 -0500

    pantalla Completa

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,6 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<script>
-		window.scrollTo(0,1);
-	</script>
 </main>
 
 <style>
