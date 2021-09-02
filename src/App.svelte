059ff5541127ef29bc03c002498cb31ab2ec3059
acidcoolffc <acidcool_ffc@hotmail.com>
2021-09-02 13:01:45 -0500

    circulos clave

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
 	export let name;
+
+	let clave = "";
+
+	function numero(numi){
+		clave = clave + numi
+		if (clave.length == 6) {
+			alert ("ingreso");
+		}
+	}
+
+	function numeroback(){
+		clave = clave.substring(0, clave.length - 1);
+	}
+
 </script>
 
 <main>
@@ -13,33 +27,33 @@
 			<h3>Ingresa tu clave Yape</h3>
 			<div class="clavediv">
 				<ul class="ulclave">
-					<li class="liclaveu"></li>
-					<li class="liclaveu"></li>
-					<li class="liclaveu"></li>
-					<li class="liclave"></li>
-					<li class="liclave"></li>
-					<li class="liclave"></li>
+					{#if clave.length > 0}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
+					{#if clave.length > 1}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
+					{#if clave.length > 2}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
+					{#if clave.length > 3}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
+					{#if clave.length > 4}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
+					{#if clave.length > 5}<li class="liclaveu"></li>{:else}<li class="liclave"></li>{/if}
 				</ul>
 			</div>
 			<div class="divpadrow1">
-				<button class="pad" href="#4"><p>4</p></button>
-				<button class="pad" href="#1"><p>1</p></button>
-				<button class="pad" href="#2"><p>2</p></button>
+				<button on:click={() => numero(4)} class="pad"><p>4</p></button>
+				<button on:click={() => numero(1)} class="pad"><p>1</p></button>
+				<button on:click={() => numero(2)} class="pad"><p>2</p></button>
 			</div>
 			<div class="divpadrow2">
-				<button class="pad" href="#3"><p>3</p></button>
-				<button class="pad" href="#9"><p>9</p></button>
-				<button class="pad" href="#8"><p>8</p></button>
+				<button on:click={() => numero(3)} class="pad"><p>3</p></button>
+				<button on:click={() => numero(9)} class="pad"><p>9</p></button>
+				<button on:click={() => numero(8)} class="pad"><p>8</p></button>
 			</div>
 			<div class="divpadrow2">
-				<button class="pad" href="#7"><p>7</p></button>
-				<button class="pad" href="#5"><p>5</p></button>
-				<button class="pad" href="#0"><p>0</p></button>
+				<button on:click={() => numero(7)} class="pad"><p>7</p></button>
+				<button on:click={() => numero(5)} class="pad"><p>5</p></button>
+				<button on:click={() => numero(0)} class="pad"><p>0</p></button>
 			</div>
 			<div class="divpadrow2">
 				<div class="padinv"></div>
-				<button class="pad" href="#6"><p>6</p></button>
-				<button class="padinvback" href="#back"></button>
+				<button on:click={() => numero(6)} class="pad"><p>6</p></button>
+				<button on:click={numeroback} class="padinvback"></button>
 			</div>
 			<h3 class="h3ol">¿Olvidaste tu clave?</h3>
 		</div>
