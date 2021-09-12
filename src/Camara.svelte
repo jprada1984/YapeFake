e871d9ef7022dd00b0d96f19875c67f855031ed5
acidcoolffc <acidcool_ffc@hotmail.com>
2021-09-12 11:47:38 -0500

    camera

--- src/Camara.svelte
+++ src/Camara.svelte
@@ -14,21 +14,21 @@
 		push('/Camara');
 	}
 
-    
-
-      let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content) {
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    scanner.addListener('scan', function (content) {
         console.log(content);
-      });
-      Instascan.Camera.getCameras().then(function (cameras) {
+        msgbox(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+            scanner.start(cameras[0]);
         } else {
-          console.error('No cameras found.');
+            console.error('No cameras found.');
+            msgbox('No cameras found.');
         }
-      }).catch(function (e) {
+    }).catch(function (e) {
         console.error(e);
-      });
+    });
 </script>
 
 <div class="divprincipal">
