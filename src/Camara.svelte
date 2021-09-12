1120d2c13292d3e5609a43eb8218b048362a0fe9
acidcoolffc <acidcool_ffc@hotmail.com>
2021-09-12 13:42:26 -0500

    camera funciona

--- src/Camara.svelte
+++ src/Camara.svelte
@@ -1,16 +1,24 @@
+<script>
+    import { onMount } from 'svelte';
+
+	onMount(async () => {
+		let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+        scanner.addListener('scan', function (content) {
+            console.log(content);
+            msgbox(content);
+        });
+        Instascan.Camera.getCameras().then(function (cameras) {
+            if (cameras.length > 0) {
+                scanner.start(cameras[0]);
+            } else {
+                console.error('No cameras found.');
+            }
+        }).catch(function (e) {
+                console.error(e);
+        });
+	});
+</script>
+
 <div class="divcamara">
     <video id="preview"></video>
-    <script type="text/javascript">
-      import Instascan from '@mathewparet/instascan'
-      var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-      scanner.addListener('scan', function (content, image) {
-        console.log(content);
-      });
-
-      Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-          scanner.start(cameras[0]);
-        }
-      });
-    </script>
 </div>
