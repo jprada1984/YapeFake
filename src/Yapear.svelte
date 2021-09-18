2ca704deb3a380c26e4e8437e8ce40e8ab7634d5
acidcoolffc <acidcool_ffc@hotmail.com>
2021-09-18 13:55:04 -0500

    nombre yapero

--- src/Yapear.svelte
+++ src/Yapear.svelte
@@ -1,6 +1,12 @@
 <script>
     import { QRcodigo } from './stores.js';
+    export let nombreya = new String ("");
+    let numnom = ""
+    nombreya = $QRcodigo;
+    nombreya = nombreya.slice(14,-16);
+    numnom = nombreya.slice(0,2);
+    nombreya = nombreya.slice(parseInt(numnom)+2+25);
 </script>
 <div class="divyapear">
-    {$QRcodigo}
+    {nombreya}
 </div>
